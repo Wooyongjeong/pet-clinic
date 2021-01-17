ac6daec389c0b8c72ff0f2084a98de95696aecdf
wooyongjeong <dndyd1231@gmail.com>
2021-01-17 11:29:37 +0900

    OwnersController 추가
    * /owners 에 대응되는 OwnersController를 추가하였습니다

--- src/main/java/com/spring/petclinic/controller/OwnersController.java
+++ src/main/java/com/spring/petclinic/controller/OwnersController.java
@@ -1,8 +1,11 @@
 package com.spring.petclinic.controller;
 
+import com.spring.petclinic.domain.Owner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OwnersController {
@@ -10,5 +13,23 @@ public class OwnersController {
     public String findOwners(Model model) {
         model.addAttribute("currentPage", "findOwners");
         return "owners/findOwners";
+    }
+
+    @PostMapping("/owners/find")
+    public String findOwnersByName(@RequestParam("name") String name, Model model) {
+        System.out.println(name);
+        return "redirect:/";
+    }
+
+    @GetMapping("/owners/new")
+    public String createForm() {
+        return "owners/createForm";
+    }
+
+    @PostMapping("/owners/new")
+    public String create(OwnerForm form) {
+        Owner owner = new Owner();
+
+        return "redirect:/";
     }
 }
