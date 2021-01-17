452000592813fe1c75c9eb2a4ff5aba67cbe7220
wooyongjeong <dndyd1231@gmail.com>
2021-01-17 13:35:52 +0900

    Add ownerService for create owner
    * owner 테이블에 데이터를 추가하기 위해 ownerService를 사용하였습니다
    * owner 테이블에 데이터를 추가하는 로직을 작성하였습니다

--- src/main/java/com/spring/petclinic/controller/OwnersController.java
+++ src/main/java/com/spring/petclinic/controller/OwnersController.java
@@ -1,14 +1,25 @@
 package com.spring.petclinic.controller;
 
 import com.spring.petclinic.domain.Owner;
+import com.spring.petclinic.service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class OwnersController {
+    private OwnerService ownerService;
+
+    @Autowired
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping("/owners/find")
     public String findOwners(Model model) {
         model.addAttribute("currentPage", "findOwners");
@@ -28,8 +39,8 @@ public class OwnersController {
 
     @PostMapping("/owners/new")
     public String create(OwnerForm form) {
-        Owner owner = new Owner();
-
+        Owner owner = new Owner(form);
+        ownerService.join(owner);
         return "redirect:/";
     }
 }
