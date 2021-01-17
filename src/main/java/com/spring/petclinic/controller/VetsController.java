c299311f46ba26fddd093fa73759eceaa0cde5e9
wooyongjeong <dndyd1231@gmail.com>
2021-01-17 14:55:48 +0900

    Add VetsController for handling /vets url mapping

---
+++ src/main/java/com/spring/petclinic/controller/VetsController.java
@@ -0,0 +1,28 @@
+package com.spring.petclinic.controller;
+
+import com.spring.petclinic.domain.Vets;
+import com.spring.petclinic.service.VetsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class VetsController {
+    private VetsService vetsService;
+
+    @Autowired
+    public VetsController(VetsService vetsService) {
+        this.vetsService = vetsService;
+    }
+
+    @GetMapping("/vets")
+    public String vets(Model model) {
+        List<Vets> vets = vetsService.findVets();
+        model.addAttribute("currentPage", "vets");
+        model.addAttribute("vetsList", vets);
+        return "vets/vetList";
+    }
+}
