b48faa2934e71659482fb779476cf7f09392e43c
wooyongjeong <dndyd1231@gmail.com>
2021-01-17 15:00:30 +0900

    Add VetsRepository and VetsService for DI

--- src/main/java/com/spring/petclinic/SpringConfig.java
+++ src/main/java/com/spring/petclinic/SpringConfig.java
@@ -1,7 +1,9 @@
 package com.spring.petclinic;
 
 import com.spring.petclinic.repository.OwnerRepository;
+import com.spring.petclinic.repository.VetsRepository;
 import com.spring.petclinic.service.OwnerService;
+import com.spring.petclinic.service.VetsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +12,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
     private final OwnerRepository ownerRepository;
+    private final VetsRepository vetsRepository;
 
     @Autowired
-    public SpringConfig(OwnerRepository ownerRepository) {
+    public SpringConfig(OwnerRepository ownerRepository, VetsRepository vetsRepository) {
         this.ownerRepository = ownerRepository;
+        this.vetsRepository = vetsRepository;
     }
 
     @Bean
     public OwnerService ownerService() {
         return new OwnerService(ownerRepository);
+    }
+
+    @Bean
+    public VetsService vetsService() {
+        return new VetsService(vetsRepository);
     }
 }
