5300151ba97e7ae76bca5cd188ce4b8985a605b7
wooyongjeong <dndyd1231@gmail.com>
2021-01-17 11:38:30 +0900

    SpringDataJpaOwnerRepository 인터페이스 추가
    * JpaRepository를 상속받은 SpringDataJpaOwnerRepository 인터페이스를 추가하였습니다. 이를 통해 Owner 엔티티에 대한 CRUD를 이용합니다.

---
+++ src/main/java/com/spring/petclinic/repository/SpringDataJpaOwnerRepository.java
@@ -0,0 +1,11 @@
+package com.spring.petclinic.repository;
+
+import com.spring.petclinic.domain.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaOwnerRepository extends JpaRepository<Owner, Long>, OwnerRepository {
+    @Override
+    Optional<Owner> findByName(String name);
+}
