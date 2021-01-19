787b048c5b5dfc5ff70e33caa8424660d5db532c
wooyongjeong <dndyd1231@gmail.com>
2021-01-19 16:43:14 +0900

    Make initialize db automatically when server starts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into owner(name, address, phone) values('정우용', '경기 성남시 분당구 불정로 6', '01012345678');
+insert into owner(name, address, phone) values('Wooyong Jeong', '경기 성남시 분당구 판교역로 235 에이치스퀘어 엔동', '01056781234');
+
+insert into vet(name, specialty) values('James Carter', 'none');
+insert into vet(name, specialty) values('Helen Leary', 'radiology');
+insert into vet(name, specialty) values('Linda Douglas', 'dentistry surgery');
+insert into vet(name, specialty) values('Rafael Ortega', 'surgery');
+insert into vet(name, specialty) values('Henry Stevens', 'radiology');
+insert into vet(name, specialty) values('Sharon Jenkins', 'none');
