6a6e09dad9e0feef01ff991664868f2b98e2bcf8
wooyongjeong <dndyd1231@gmail.com>
2021-01-25 15:57:48 +0900

    Update data.sql to English

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,15 @@
-insert into owner(name, address, phone) values('홍길동', '경기 성남시 분당구 불정로 6', '01012345678');
-insert into owner(name, address, phone) values('John Doe', '경기 성남시 분당구 판교역로 235 에이치스퀘어 엔동', '01056781234');
+insert into owner(name, address, phone) values('Jane Smith', 'Seoul', '01012345678');
+insert into owner(name, address, phone) values('John Doe', 'Busan', '01056781234');
 
-insert into pet(name, birth_date, type, owner_id) values('솜이', '2020-11-11', '강아지', '1');
-insert into pet(name, birth_date, type, owner_id) values('치즈', '2020-08-22', '고양이', '2');
+insert into pet(name, birth_date, type, owner_id) values('Choco', '2020-11-11', 'Dog', '1');
+insert into pet(name, birth_date, type, owner_id) values('Vanilla', '2020-11-11', 'Dog', '1');
+insert into pet(name, birth_date, type, owner_id) values('Cheese', '2020-08-22', 'Cat', '2');
+insert into pet(name, birth_date, type, owner_id) values('Oreo', '2020-08-22', 'Cat', '2');
 
-insert into visit(visit_date, description, pet_id) values('2020-11-18', '예방접종', '1');
-insert into visit(visit_date, description, pet_id) values('2020-08-23', '혈액검사 등', '2');
+insert into visit(visit_date, description, pet_id) values('2020-11-18', 'Vaccination', '1');
+insert into visit(visit_date, description, pet_id) values('2020-11-18', 'Vaccination', '2');
+insert into visit(visit_date, description, pet_id) values('2020-08-30', 'Blood test', '3');
+insert into visit(visit_date, description, pet_id) values('2020-08-30', 'Blood test', '4');
 
 insert into vet(name, specialty) values('James Carter', 'none');
 insert into vet(name, specialty) values('Helen Leary', 'radiology');
